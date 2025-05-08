48be1b3446215cd712c104a2a0c431b5b173cb51
Samridhi Kakkar <kakkar4@purdue.edu>
2025-05-08 06:43:23 -0400

    Create MarketplaceServerInterface.java

---
+++ src/MarketplaceServerInterface.java
@@ -0,0 +1,20 @@
+/**
+ * PHASE 3 PROJECT MARKETPLACE
+ *
+ * @author Samridhi
+ * @version 7/5/25
+ */
+public interface MarketplaceServerInterface {
+    // since the methods in the marketplaceserver
+    // were private thus
+    // we could not implement them in
+    // the interface here
+    public static final String PRODUCTS_DIR = "products/";
+    public static final String CHATS_DIR = "chats/";
+    public static final String CLIENTS_FILE = "clients.txt";
+    public static final String SELLERS_FILE = "sellers.txt";
+    public static final String BALANCES_FILE = "balances.txt";
+    public static final String TRANSACTIONS_FILE = "transactions.txt";
+    public static final String IMAGE_DIR = "images/";
+
+}
